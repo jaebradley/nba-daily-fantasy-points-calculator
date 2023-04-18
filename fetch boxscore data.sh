c453eaed98097a04a8735c7f1a93a250408e912d
Jae Bradley <jae.b.bradley@gmail.com>
2023-04-17 21:45:58 -0400

    Fetching boxscores data test

--- fetch boxscore data.sh
+++ fetch boxscore data.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 function fetch_boxscore_data() {
+  if [[ "$#" != "1" ]]; then printf "Expected a single argument - an NBA.com game id\n" && exit 255; fi
   local -r game_id="$1"
-  local -r jq_executable_path="$2"
 
-  # Has to be absolute path to binary
-  if [[ ! -f "${jq_executable_path}" ]]; then printf "${jq_executable_path} is not a file\n" && exit 255; fi
-  if [[ ! -e "${jq_executable_path}" ]]; then printf "${jq_executable_path} is not executable\n" && exit 255; fi
+  command -v "curl" &> /dev/null
+  if [[ "$?" != "0" ]]; then printf "curl program does not exist\n" && exit 255; fi
+
   curl --silent "https://cdn.nba.com/static/json/liveData/boxscore/boxscore_${game_id}.json" 
 }
