d7c602181a6ce9214cb0b6b4ae3ec8795aef848c
Jae Bradley <jae.b.bradley@gmail.com>
2023-04-22 15:52:37 -0400

    Fix invalid numeric literal (#6)

--- main.sh
+++ main.sh
@@ -11,17 +11,18 @@ function main() {
   local -r month="$2"
   local -r day="$3"
   local -r jq_executable_path="$4"
-  read -a game_ids < <(fetch_games_for_day "${year}" "${month}" "${day}" | parse_daily_games "${jq_executable_path}" | "${jq_executable_path}" 'map(.gameId) | .[]' | xargs -L1)
 
-  for game_id in "${game_ids[@]}"
+  while IFS=$"\n", read -r -a game_id
   do
     while IFS=$",", read -r -a player_boxscore
     do
       local fantasy_points
       fantasy_points=$(calculate_classic_points "${player_boxscore[@]:2}")
+      if [[ $? -ne 0 ]]; then printf "Could not calculate fantasy points\n" && exit 255; fi
+
       printf "${player_boxscore[0]} | ${fantasy_points} | ${player_boxscore[1]}\n"
-    done  < <(fetch_boxscore_data "${game_id}" "${jq_executable_path}" | parse_boxscore_data "${jq_executable_path}")
-  done
+    done  < <(fetch_boxscore_data "${game_id}" | parse_boxscore_data "${jq_executable_path}")
+  done < <(fetch_games_for_day "${year}" "${month}" "${day}" | parse_daily_games "${jq_executable_path}" | "${jq_executable_path}" 'map(.gameId) | .[]' | xargs -L1)
 }
 
 main "$@"
