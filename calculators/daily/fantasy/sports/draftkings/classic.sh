1202ff25922c5935ddb07847bf7c7fe14e4ec45b
Jae Bradley <jae.b.bradley@gmail.com>
2023-04-24 23:05:55 -0400

    Fix point calculator

--- calculators/daily/fantasy/sports/draftkings/classic.sh
+++ calculators/daily/fantasy/sports/draftkings/classic.sh
@@ -2,6 +2,8 @@
 # https://www.draftkings.com/help/rules/4
 
 function calculate_classic_points() {
+  if [[ "$#" != "7" ]]; then printf "Expected 7 arguments: assists, blocks, points, rebounds, steals, three pointers made, and turnovers" && exit 255; fi
+
   local -r assists="$1"
   local -r blocks="$2"
   local -r points="$3"
@@ -11,19 +13,19 @@ function calculate_classic_points() {
   local -r turnovers="$7"
 
   local double_digit_categories_count=0
-  if [[ "${assists}" > 9 ]]; then ((++double_digit_categories_count)); fi
-  if [[ "${points}" > 9 ]]; then ((++double_digit_categories_count)); fi
-  if [[ "${blocks}" > 9 ]]; then ((++double_digit_categories_count)); fi
-  if [[ "${rebounds}" > 9 ]]; then ((++double_digit_categories_count)); fi
-  if [[ "${steals}" > 9 ]]; then ((++double_digit_categories_count)); fi
+  if [[ assists -gt 9 ]]; then ((++double_digit_categories_count)); fi
+  if [[ points -gt 9 ]]; then ((++double_digit_categories_count)); fi
+  if [[ blocks -gt 9 ]]; then ((++double_digit_categories_count)); fi
+  if [[ rebounds -gt 9 ]]; then ((++double_digit_categories_count)); fi
+  if [[ steals -gt 9 ]]; then ((++double_digit_categories_count)); fi
 
   local double_digit_categories_bonus=0
-  if [[ "${double_digit_categories_count}" == "2" ]]; then
+  if [[ double_digit_categories_count -eq 2 ]]; then
     double_digit_categories_bonus=1
-  elif [[ "${double_digit_categories_count}" > 2 ]]; then
+  elif [[ double_digit_categories_count -gt 2 ]]; then
     double_digit_categories_bonus=3
   fi
   
-  printf "${points}+(${three_pointers_made}*0.5)+(${rebounds}*1.25)+(${assists}*1.5)+(${steals}*2)+(${blocks}*2)+(${turnovers}*0.5)+${double_digit_categories_bonus}; scale=2" | bc
+  printf "${points}+(${three_pointers_made}*0.5)+(${rebounds}*1.25)+(${assists}*1.5)+(${steals}*2)+(${blocks}*2)-(${turnovers}*0.5)+(${double_digit_categories_bonus}*1.5); scale=4" | bc
 }
 
