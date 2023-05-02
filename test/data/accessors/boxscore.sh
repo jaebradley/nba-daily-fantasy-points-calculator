689abd037dd517690d776bed9fceaa206675d333
Jae Bradley <jae.b.bradley@gmail.com>
2023-05-02 11:05:50 -0400

    Reorganize

--- test/data/accessors/boxscore.sh
+++ test/data/accessors/boxscore.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-source "fetch boxscore data.sh"
+. "$(dirname "${BASH_SOURCE[0]}")/../../../data/accessors/boxscore.sh"
 
 main() {
   local -r boxscore_data=$(fetch_boxscore_data "0022201201")
   if [[ "$?" != 0 ]]; then printf "Failed to fetch boxscore data\n" && exit 255; fi
 
-  local -r expected_boxscore_data=$(cat "test/data/boxscores/0022201201.json")
+  local -r expected_boxscore_data=$(cat "$(dirname "${BASH_SOURCE[0]}")/../boxscores/0022201201.json")
   if [[ "${boxscore_data}" != "${expected_boxscore_data}" ]]; then printf "Expected boxscore data is not equal to fetched boxscore data\n" && exit 255; fi
 }
 
--- test/data/parsers/boxscore.sh
+++ test/data/parsers/boxscore.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. "$(dirname "${BASH_SOURCE[0]}")/../parse boxscore data.sh"
+. "$(dirname "${BASH_SOURCE[0]}")/../../../data/parsers/boxscore.sh"
 
 function main() {
   if [[ "$#" != "1" ]]; then printf "Expected 1 argument - the path to the jq program" && exit 255; fi
@@ -14,11 +14,11 @@ function main() {
 
   for input_file_name in "${input_file_names[@]}"
   do
-    local input_file_path="${current_directory}/data/boxscores/${input_file_name}.json"
+    local input_file_path="${current_directory}/../boxscores/${input_file_name}.json"
     if [[ ! -f "${input_file_path}" ]]; then printf "Input file at ${input_file_path} is not a regular file\n" && exit 255; fi
     if [[ ! -r "${input_file_path}" ]]; then printf "Input file at ${input_file_path} is not readable\n" && exit 255; fi
 
-    local expected_output_file_path="${current_directory}/data/expected/parse boxscore data/${input_file_name}.csv"
+    local expected_output_file_path="${current_directory}/expected/boxscore/${input_file_name}.csv"
     if [[ ! -f "${expected_output_file_path}" ]]; then printf "Expected output file at ${expected_output_file_path} is not a regular file\n" && exit 255; fi
     if [[ ! -r "${expected_output_file_path}" ]]; then printf "Expected output file at ${expected_output_file_path} is not readable\n" && exit 255; fi
     local expected_output
