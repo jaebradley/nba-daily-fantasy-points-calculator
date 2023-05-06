f94e5fee4e47ee465acafed48c8c44da2d8447bb
Jae Bradley <jae.b.bradley@gmail.com>
2023-05-06 17:35:30 -0400

    Update

--- tests/utilities/execute test file.sh
+++ tests/utilities/execute test file.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
+. "$(dirname "${BASH_SOURCE[0]}")/../../utilities/error.sh"
+
 execute_test_file() {
-    if [[ "1" != "$#" ]]; then printf "Expected a single path to file argument\n" && exit 255; fi
+    if [[ "1" != "$#" ]]; then fail "Expected a single path to file argument\n"; fi
 
     local -r file_path="$1";
 
-    if [[ ! -f "${file_path}" ]]; then printf "File ${file_path} is not a regular file\n" && exit 255; fi
-    if [[ ! -x "${file_path}" ]]; then printf "File ${file_path} is not executable\n" && exit 255; fi
+    if [[ ! -f "${file_path}" ]]; then fail "File ${file_path} is not a regular file\n"; fi
+    if [[ ! -x "${file_path}" ]]; then fail "File ${file_path} is not executable\n"; fi
 
     printf "Starting ${file_path}\n"
-    time "${file_path}"
+    time "${file_path}" || fail "Could not time script at ${file_path}"
     local -r exit_code="$?"
     printf "Finished ${file_path} with exit code: ${exit_code}\n"
-    if [[ "0" != "${exit_code}" ]]; then printf "Tests failed for ${file_path}\n"; fi
+    if [[ "0" != "${exit_code}" ]]; then fail"Tests failed for ${file_path}\n"; fi
 }
