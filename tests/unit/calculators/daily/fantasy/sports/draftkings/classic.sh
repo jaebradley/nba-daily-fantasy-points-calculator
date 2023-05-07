199af2ce7c51b9f217fcd75114c20b3aba052731
Jae Bradley <jae.b.bradley@gmail.com>
2023-05-06 21:52:15 -0400

    Update

--- tests/unit/calculators/daily/fantasy/sports/draftkings/classic.sh
+++ tests/unit/calculators/daily/fantasy/sports/draftkings/classic.sh
@@ -3,36 +3,24 @@
 . "$(dirname "${BASH_SOURCE[0]}")/../../../../../../../calculators/daily/fantasy/sports/draftkings/classic.sh"
 . "$(dirname "${BASH_SOURCE[0]}")/../../../../../../../utilities/error.sh"
 
+compare() {
+  local output 
+  output=$(calculate_classic_points "${@: 1:7}")
+  if [[ $? -ne 0 ]]; then fail "Could not calculate points\n"; fi
+
+  local difference
+  difference=$(bc <<< "${@: 8:8} - ${output}")
+  if [[ $? -ne 0 ]]; then fail "Could not calculate difference between expected and outputted points\n"; fi
+  if [[ "0" != "${difference}" ]]; then fail "Difference is ${difference} and not 0\n"; fi
+}
+
 function main() {
-  local all_zeroes_output
-  all_zeroes_output=$(calculate_classic_points "0" "0" "0" "0" "0" "0" "0")
-  if [[ $? -ne 0 ]]; then fail "Could not calculate points\n"; fi
-  if [[ "0" != "${all_zeroes_output}" ]]; then fail "Expected 0 but instead got ${all_zeroes_output}"; fi
-
-  local only_turnovers_output
-  only_turnovers_output=$(calculate_classic_points "0" "0" "0" "0" "0" "0" "1")
-  if [[ $? -ne 0 ]]; then fail "Could not calculate points\n"; fi
-  if [[ "-.5" != "${only_turnovers_output}" ]]; then fail "Expected -0.5 but instead got ${only_turnovers_output}"; fi
-
-  local one_of_everything
-  one_of_everything=$(calculate_classic_points "1" "1" "1" "1" "1" "1" "1")
-  if [[ $? -ne 0 ]]; then fail "Could not calculate points\n"; fi
-  if [[ "7.75" != "${one_of_everything}" ]]; then fail "Expected 6.75 but instead got ${one_of_everything}"; fi
-
-  local double_double
-  double_double=$(calculate_classic_points "10" "10" "0" "0" "0" "0" "0")
-  if [[ $? -ne 0 ]]; then fail "Could not calculate points\n"; fi
-  if [[ "36.5" != "${double_double}" ]]; then fail "Expected 27.5 but instead got ${double_double}"; fi
-
-  local triple_double
-  triple_double=$(calculate_classic_points "10" "10" "10" "0" "0" "0" "0")
-  if [[ $? -ne 0 ]]; then fail "Could not calculate points\n"; fi
-  if [[ "49.5" != "${triple_double}" ]]; then fail "Expected 48 but instead got ${triple_double}"; fi
-
-  local quadruple_double
-  quadruple_double=$(calculate_classic_points "10" "10" "10" "10" "0" "0" "0")
-  if [[ $? -ne 0 ]]; then fail "Could not calculate points\n"; fi
-  if [[ "62.00" != "${quadruple_double}" ]]; then fail "Expected 62 but instead got ${quadruple_double}"; fi
+  compare "0" "0" "0" "0" "0" "0" "0" "0" || fail "Could not calculate all zeroes"
+  compare "0" "0" "0" "0" "0" "0" "1" "-0.5" || fail "Could not calculate only turnovers"
+  compare "1" "1" "1" "1" "1" "1" "1" "7.75" || fail "Could not calculate one of everything"
+  compare "10" "10" "0" "0" "0" "0" "0" "36.5" || fail "Could not calculate double double"
+  compare "10" "10" "10" "0" "0" "0" "0" "49.5" || fail "Could not calculate double double"
+  compare "10" "10" "10" "10" "0" "0" "0" "62" || fail "Could not calculate double double"
 }
 
 main
