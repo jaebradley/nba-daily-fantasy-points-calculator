0e13a8c0eacec63a8eb8b322c16dde0f105194a9
Jae Bradley <jae.b.bradley@gmail.com>
2023-05-06 21:20:27 -0400

    Update

--- utilities/error.sh
+++ utilities/error.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 fail() {
-  if [[ 1 -ne $# ]]; then printf "Expected a single argument - error message to output to standard output" && exit 255; fi
-  printf "$1" && exit 255
+  if [[ 1 -ne $# ]]; then printf "%s" "Expected a single argument - error message to output to standard output" && exit 255; fi
+  printf "%s" "$1" && exit 255
 }
