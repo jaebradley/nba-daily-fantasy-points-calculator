a225b92d743a3f6bc1e737151a2703be2ddf8b35
Jae Bradley <jae.b.bradley@gmail.com>
2023-05-11 11:22:29 -0400

    Install shellcheck

--- build.sh
+++ build.sh
@@ -5,14 +5,15 @@
 . "$(dirname "${BASH_SOURCE[0]}")/utilities/error.sh"
 
 build() {
-  if [[ 3 -ne $# ]]; then fail "Expected three arguments: the path to the jq binary, the expected jq version, and the path to the test directory"; fi
+  if [[ 4 -ne $# ]]; then fail "Expected four arguments: the path to the jq binary, the expected jq version, the path to the shellcheck program, and the path to the test directory"; fi
 
   local -r jq_path="$1"
   local -r expected_version="$2"
-  local -r test_directory_path="$3"
+  local -r shellcheck_path="$3"
+  local -r test_directory_path="$4"
 
-  install_dependencies "${jq_path}" "${expected_version}" || fail "Failed to install dependencies\n"
+  install_dependencies "${jq_path}" "${expected_version}" "${shellcheck_path}" || fail "Failed to install dependencies\n"
   run_tests "${test_directory_path}" || fail "Failed to run tests in ${test_directory_path}\n"
 }
 
-build "$(dirname "${BASH_SOURCE[0]}")/.dependencies/bin/jq" "jq-1.6" "$(dirname "${BASH_SOURCE[0]}")/tests"
+build "$(dirname "${BASH_SOURCE[0]}")/.dependencies/bin/jq" "jq-1.6" "$(dirname "${BASH_SOURCE[0]}")/.dependencies/bin/shellcheck" "$(dirname "${BASH_SOURCE[0]}")/tests"
---
+++ build/shellcheck/install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+declare current_directory
+current_directory=$(dirname "${BASH_SOURCE[0]}")
+if [[ 0 -ne $? ]]; then printf "Unable to calculate the current directory" && exit 255; fi
+
+. "${current_directory}/../../utilities/error.sh" || exit 255
+
+
+install_shellcheck() {
+  if [[ 2 -ne $# ]]; then fail "Expected two arguments: the URL for the jq binary to install and the installation location for the jq binary\n"; fi
+
+  local -r binary_url="$1"
+  local -r installation_location="$2"
+
+  command -v "curl" &> /dev/null || fail "curl program does not exist\n"
+  command -v "tar" &> /dev/null || fail "tar program does not exist\n"
+
+  local temporary_file_path
+  temporary_file_path="/tmp/$(uuidgen)"
+  if [[ $? -ne 0 ]]; then fail "Could not generate temporary file path\n"; fi
+
+  mkdir -p "${temporary_file_path}" || fail "Could not create temporary file at path: ${temporary_file_path}\n"
+  curl --silent --location "${binary_url}" --output "${temporary_file_path}/_" || fail "Unable to download binary\n"
+
+  tar --extract --verbose --gunzip --file "${temporary_file_path}/_" -C "$temporary_file_path" || fail "Unable to extract binary\n"
+
+  local installation_directory
+  installation_directory=$(dirname "${installation_location}")
+  if [[ $? -ne 0 ]]; then fail "Could not identify installation directory for: ${installation_location}\n"; fi
+
+  mkdir -p "${installation_directory}" || fail "Could not create directory at path: ${installation_directory}\n"
+
+  mv "${temporary_file_path}/shellcheck-v0.9.0/shellcheck" "${installation_location}" || fail "Could not move jq binary from ${temporary_file_path} to ${installation_location}\n"
+
+  chmod 755 "${installation_location}" || fail "Could not update binary permissions at ${installation_location}\n"
+}
+
