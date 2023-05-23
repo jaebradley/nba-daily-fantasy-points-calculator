6666763029b9129760157856375b869c0ce71fe9
Jae Bradley <jae.b.bradley@gmail.com>
2023-05-23 18:08:12 -0400

    Initial

--- tests/integration/main.sh
+++ tests/integration/main.sh
@@ -4,8 +4,7 @@
 
 main() {
   local file_directory
-  file_directory=$(dirname "${BASH_SOURCE[0]}")
-  if [[ 0 -ne $? ]]; then fail "Unable to calculate current file directory\n"; fi
+  if ! file_directory=$(dirname "${BASH_SOURCE[0]}"); then fail "Unable to calculate current file directory\n"; fi
 
   "${file_directory}/../../main.sh" "2023" "05" 06" || fail "Could not calculate points for May 6th, 2023"
   "${file_directory}/../../main.sh" "2023" "05" 06" "${file_directory}/../../.dependencies/bin/jq" || fail "Could not calculate points for May 6th, 2023"
