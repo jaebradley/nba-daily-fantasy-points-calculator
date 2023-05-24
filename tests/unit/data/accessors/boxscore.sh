13018a6493f09df27a36389b6f2dd75bb47debf4
Jae Bradley <jae.b.bradley@gmail.com>
2023-05-23 23:01:45 -0400

    Update

--- tests/unit/data/accessors/boxscore.sh
+++ tests/unit/data/accessors/boxscore.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-. "$(dirname "${BASH_SOURCE[0]}")/../../../../data/accessors/boxscore.sh"
+. "$(dirname "${BASH_SOURCE[0]}")/../../../../utilities/error.sh" || exit 255
+. "$(dirname "${BASH_SOURCE[0]}")/../../../../data/accessors/boxscore.sh" || fail "Unable to import boxscore accessor"
 
 main() {
-  local -r boxscore_data=$(fetch_boxscore_data "0022201201")
-  if [[ "$?" != 0 ]]; then printf "Failed to fetch boxscore data\n" && exit 255; fi
+  local boxscore_data
+  if ! boxscore_data=$(fetch_boxscore_data "0022201201"); then fail "Failed to fetch boxscore data\n"; fi
 
-  local -r expected_boxscore_data=$(cat "$(dirname "${BASH_SOURCE[0]}")/../boxscores/0022201201.json")
+  local expected_boxscore_data
+  if ! expected_boxscore_data=$(cat "$(dirname "${BASH_SOURCE[0]}")/../boxscores/0022201201.json"); then fail "Unable to read expected boxscore data\n"; fi
+
   if [[ "${boxscore_data}" != "${expected_boxscore_data}" ]]; then printf "Expected boxscore data is not equal to fetched boxscore data\n" && exit 255; fi
 }
 
