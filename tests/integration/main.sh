69fe0f1a0cb3742aeb6535413615874ea034c18b
Jae Bradley <jae.b.bradley@gmail.com>
2023-05-28 19:38:55 -0400

    Add integration tests

--- tests/integration/main.sh
+++ tests/integration/main.sh
@@ -8,10 +8,10 @@ main() {
 
   local -r executable_script_path="${file_directory}/../../main.sh";
 
-  "${executable_script_path}" 2023 05 06 || fail "Could not calculate points for May 6th, 2023"
+  "${executable_script_path}" 2023 05 06 &> /dev/null || fail "Could not calculate points for May 6th, 2023 using default jq program"
 
   local -r jq_executable_path="${file_directory}/../../.dependencies/bin/jq"
-  "${executable_script_path}" 2023 05 06 "${jq_executable_path}" || fail "Could not calculate points for May 6th, 2023"
+  "${executable_script_path}" 2023 05 06 "${jq_executable_path}"  &> /dev/null || fail "Could not calculate points for May 6th, 2023using specified jq program"
 }
 
 main
