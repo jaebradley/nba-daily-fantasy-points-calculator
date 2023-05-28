90e7476743db5abc1bdf605e61dcf33337b9c88f
Jae Bradley <jae.b.bradley@gmail.com>
2023-05-28 00:06:24 -0400

    Resolve all Shellcheck violations (#36)

--- tests/integration/main.sh
+++ tests/integration/main.sh
@@ -6,8 +6,12 @@ main() {
   local file_directory
   if ! file_directory=$(dirname "${BASH_SOURCE[0]}"); then fail "Unable to calculate current file directory\n"; fi
 
-  "${file_directory}/../../main.sh" "2023" "05" 06" || fail "Could not calculate points for May 6th, 2023"
-  "${file_directory}/../../main.sh" "2023" "05" 06" "${file_directory}/../../.dependencies/bin/jq" || fail "Could not calculate points for May 6th, 2023"
+  local -r executable_script_path="${file_directory}/../../main.sh";
+
+  "${executable_script_path}" 2023 05 06 || fail "Could not calculate points for May 6th, 2023"
+
+  local -r jq_executable_path="${file_directory}/../../.dependencies/bin/jq"
+  "${executable_script_path}" 2023 05 06 "${jq_executable_path}" || fail "Could not calculate points for May 6th, 2023"
 }
 
 main
